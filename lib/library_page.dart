0d2823d602938ff280098a98b66627f296b5ce67
Andrey <rielyalkov1@gmail.com>
2023-04-20 21:38:18 +0300

    Add a placeholder for empty library

--- lib/library_page.dart
+++ lib/library_page.dart
@@ -25,27 +25,33 @@ class LibraryPageState extends State<LibraryPage> {
             String? savedData = data?.getString('saved_data');
             List<dynamic> optionList =
             (savedData == null) ? [] : (jsonDecode(savedData));
-            return ListView.builder(
-                itemCount: optionList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Wrap(children: [
-                    Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: Row(children: [
-                          Text(optionList[index]),
-                          IconButton(onPressed: () {
-                            setState(() {
-                              optionList.removeAt(index);
-                              data?.setString(
-                                  'saved_data', jsonEncode(optionList));
-                            });
-                          }, icon: const Icon(Icons.delete))
-                        ],),
+            if (optionList.isNotEmpty) {
+              return ListView.builder(
+                  itemCount: optionList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      child: LimitedBox(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Wrap(crossAxisAlignment: WrapCrossAlignment.center,children: [
+                            Text(optionList[index]),
+                            IconButton(onPressed: () {
+                              setState(() {
+                                optionList.removeAt(index);
+                                data?.setString(
+                                    'saved_data', jsonEncode(optionList));
+                              });
+                            }, icon: const Icon(Icons.delete))
+                          ],),
+                        ),
                       ),
-                    )
-                  ]);
-                });
+                    );
+                  });
+            } else {
+              return const Center(
+                child: Text('Пока нет сохраненных раскладок'),
+              );
+            }
           },
         ));
   }
