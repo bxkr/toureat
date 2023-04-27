d6ae0c04da2fe52f49f3dd86de4e251ccc5fd314
Andrey <rielyalkov1@gmail.com>
2023-04-27 21:57:23 +0300

    Improve stepper

--- lib/create_dialog.dart
+++ lib/create_dialog.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:toureat/create_dialog_steps/general.dart';
 
-class CreateDialog extends StatelessWidget {
-  CreateDialog({super.key, required this.onClose, required this.onDone});
+class CreateDialog extends StatefulWidget {
+  const CreateDialog({super.key, required this.onClose, required this.onDone});
 
   final void Function() onClose;
   final void Function() onDone;
+
+  @override
+  State<CreateDialog> createState() => _CreateDialogState();
+}
+
+class _CreateDialogState extends State<CreateDialog> {
   final controller = TextEditingController();
+
+  int _step = 0;
+  final List<Step> _steps = [
+    const Step(title: Text('О походе'), content: CreateGeneral()),
+    Step(title: const Text('...1'), content: Container()),
+    Step(title: const Text('...2'), content: Container()),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,18 +27,54 @@ class CreateDialog extends StatelessWidget {
       body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Stepper(
-            type: StepperType.horizontal,
+            controlsBuilder:
+                (BuildContext context, ControlsDetails controlsDetails) {
+              Function()? onStepContinue = controlsDetails.onStepContinue;
+              Function()? onStepCancel = controlsDetails.onStepCancel;
+              if (_step == 0) {
+                onStepCancel = null;
+              }
+              if (_step == _steps.length-1) {
+                onStepContinue = null;
+              }
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      (onStepContinue != null) ? OutlinedButton(
+                        onPressed: onStepContinue,
+                        child: const Text('Дальше'),
+                      ) : Container(),
+                      const SizedBox(width: 8),
+                      (onStepCancel != null) ? OutlinedButton(
+                        onPressed: onStepCancel,
+                        child: const Text('Назад'),
+                      ) : Container(),
+                    ],
+                  ),
+                );
+            },
+            currentStep: _step,
+            type: StepperType.vertical,
             elevation: 0,
-            steps: const [
-              Step(title: Text('О походе'), content: CreateGeneral())
-            ],
+            steps: _steps,
+            onStepCancel: () {
+              if (_step > 0) {
+                setState(() {
+                  _step--;
+                });
+              }
+            },
+            onStepContinue: () {
+              setState(() => _step++);
+            },
+            onStepTapped: (int index) => setState(() => _step = index),
           )),
       appBar: AppBar(
         title: const Text('Создание раскладки'),
-        leading: IconButton(onPressed: onClose, icon: const Icon(Icons.close)),
-        actions: const [
-          IconButton(onPressed: null, icon: Icon(Icons.done))
-        ],
+        leading: IconButton(
+            onPressed: widget.onClose, icon: const Icon(Icons.close)),
+        actions: const [IconButton(onPressed: null, icon: Icon(Icons.done))],
       ),
     );
   }
